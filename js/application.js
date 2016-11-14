ff5ff1e7699de6401adb3defd0432abaa9c92628
Jan-Paul Koudstaal <jan-paulkoudstaal@Jan-Pauls-MacBook-Pro.local>
2016-11-14 20:43:28 +0100

    lazy loading added

--- js/application.js
+++ js/application.js
@@ -1,14 +1,14 @@
 $(function() {
 
-	$('.waypoint').waypoint(function(direction) {
-		$(this.element).addClass('is-visible');
-		console.log(this.element);
-		// console.log(this.element.id + ' hit')
-	}, {
-		offset: '50%'
-	});
+  $('.waypoint').waypoint(function(direction) {
+    $(this.element).addClass('is-visible');
+    console.log(this.element);
+    // console.log(this.element.id + ' hit')
+  }, {
+    offset: '50%'
+  });
 
-	$('a[href*=#]:not([href=#])').click(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
